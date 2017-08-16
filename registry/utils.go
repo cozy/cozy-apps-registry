2191b2980d17bb7ee1b6114fd278acad4bd5d650
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-08-16 11:05:29 +0200

    Simplify AbsPath

--- registry/utils.go
+++ registry/utils.go
@@ -175,13 +175,6 @@ func AbsPath(inPath string) string {
 
 	if strings.HasPrefix(inPath, "~") {
 		inPath = UserHomeDir() + inPath[len("~"):]
-	} else if strings.HasPrefix(inPath, "$HOME") {
-		inPath = UserHomeDir() + inPath[len("$HOME"):]
-	}
-
-	if strings.HasPrefix(inPath, "$") {
-		end := strings.Index(inPath, string(os.PathSeparator))
-		inPath = os.Getenv(inPath[1:end]) + inPath[end:]
 	}
 
 	p, err := filepath.Abs(inPath)
