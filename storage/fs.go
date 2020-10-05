626855490e0ac7f34980066a10500b0add596aec
aeris <aeris@imirhil.fr>
2020-10-05 16:57:55 +0200

    Check for prefix before create file

--- storage/fs.go
+++ storage/fs.go
@@ -62,6 +62,11 @@ func (m *localFS) getPath(prefix base.Prefix, original string) (string, error) {
 }
 
 func (m *localFS) Create(prefix base.Prefix, name, contentType string, content io.Reader) error {
+	dir := filepath.Join(m.baseDir, string(prefix))
+	if _, err := os.Stat(dir); err != nil && os.IsNotExist(err) {
+		return base.NewFileNotFoundError(err)
+	}
+
 	path, err := m.getPath(prefix, name)
 	if err != nil {
 		return err
@@ -73,7 +78,7 @@ func (m *localFS) Create(prefix base.Prefix, name, contentType string, content i
 	}
 	f, err := os.Create(path)
 	if err != nil {
-		return err
+		return base.NewInternalError(err)
 	}
 	defer f.Close()
 	if _, err = io.Copy(f, content); err != nil {
